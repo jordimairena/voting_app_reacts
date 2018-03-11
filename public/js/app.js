4dddf27c3ca28ecd43e539fe4abc94a139539712
jordimairena <jordim95@hotmail.com>
2018-03-11 11:23:20 -0600

    sum of votes completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,33 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
   handleProductUpVote(productId) {
-  console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
   render() {
-    const products = Seed.products.sort((a, b) => (b.votes - a.votes));
-    const productComponents = Seed.products.map((product) => (
+    const products = this.state.products.sort((a, b) => (b.votes - a.votes));
+        const productComponents = products.map((product) => (
         <Product
           key={'product-' + product.id}
           id={product.id}
@@ -27,6 +50,7 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   constructor(props) {
     super(props);
+
     this.handleUpVote = this.handleUpVote.bind(this);
 }
   handleUpVote() {
